39b9428a5ea6beb050cdea4c0f0978b1abd3d7fa
msekulovic <milorad.sekulovic@gmail.com>
2017-03-22 11:38:46 +0100

    moved program interfaces made for test to ProgramExtender then inherited the programextender to the program so that it can be used, also separated in the folders for better visibility

---
+++ testInterfaces/Interfaces/InterfaceTests/TestingInterfaces.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testInterfaces.Interfaces
+{
+
+    class ProgramExtender : testVirtual, IMyInterface, IMultipleInheritance, Ijedan, Idva
+    {
+        public override void Foo()
+        {
+            Console.WriteLine("now it does what you wait it to.");
+            // and maybe the thing that it already did ?
+            base.Foo();
+        }
+
+        public void MethodToImplement()
+        {
+            Console.WriteLine("MethodToImplement() called.");
+        }
+
+        public void ParentInterfaceMethod()
+        {
+            Console.WriteLine("ParentInterfaceMethod() called.");
+        }
+
+        public int wow()
+        {
+            return 1;
+        }
+
+        //public override void AbsMethod()
+        //{
+        //    Console.WriteLine("implemented");
+        //}
+
+        public void IzJedinice()
+        {
+            Console.WriteLine("one method from one interface");
+        }
+
+        void Idva.SameName()
+        {
+            Console.WriteLine("calling samename from idva");
+        }
+
+        void Ijedan.SameName()
+        {
+            Console.WriteLine("calling samename from ijedan");
+        }
+
+        int Idva.IzJedinice()
+        {
+            Console.WriteLine("one method from two interface");
+            return 1;
+        }
+    }
+    interface IParentInterface
+    {
+        void ParentInterfaceMethod();
+    }
+    interface IMyInterface : IParentInterface
+    {
+        void MethodToImplement();
+    }
+
+    interface IMultipleInheritance
+    {
+        int wow();
+    }
+
+    internal abstract class absClass
+    {
+        public abstract void AbsMethod();
+
+        public int NonAbsMethod()
+        {
+            return 0;
+        }
+    }
+
+    interface Ijedan
+    {
+        void IzJedinice();
+
+        void SameName();
+    }
+
+    interface Idva
+    {
+        int IzJedinice();
+        void SameName();
+    }
+
+    interface IAnimal
+    {
+        string Describe();
+
+        string Name { get; set; }
+    }
+
+    class Dog : IAnimal, IComparable
+    {
+        private string name;
+        public Dog(string name)
+        {
+            this.Name = name;
+        }
+        public string Describe()
+        {
+            return "Hello, I'm a dog and my name is " + this.Name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj is IAnimal)
+                return this.Name.Equals((obj as IAnimal).Name) == true ? 1 : 0;
+            return 0;
+        }
+
+        public static bool CompareNames(object first, object second)
+        {
+            if (first is IAnimal && second is IAnimal)
+            {
+                if ((first as IAnimal).Name == (second as IAnimal).Name)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    class testVirtual
+    {
+        public virtual void Foo()
+        {
+            Console.WriteLine("i do something you might not want me to.");
+        }
+    }
+}
--- testInterfaces/Program.cs
+++ testInterfaces/Program.cs
@@ -6,106 +6,11 @@ using System.Threading.Tasks;
 using Polynomial;
 using testInterfaces.Design_Patterns.Creational;
 using testInterfaces.Design_Patterns.Structural;
+using testInterfaces.Interfaces;
 
 namespace testInterfaces
 {
-    interface IParentInterface
-    {
-        void ParentInterfaceMethod();
-    }
-    interface IMyInterface : IParentInterface
-    {
-        void MethodToImplement();
-    }
-
-    interface IMultipleInheritance
-    {
-        int wow();
-    }
-
-    internal abstract class absClass
-    {
-        public abstract void AbsMethod();
-
-        public int NonAbsMethod()
-        {
-            return 0;
-        }
-    }
-
-    interface Ijedan
-    {
-        void IzJedinice();
-
-        void SameName();
-    }
-
-    interface Idva
-    {
-        int IzJedinice();
-        void SameName();
-    }
-
-    interface IAnimal
-    {
-        string Describe();
-
-        string Name { get; set; }
-    }
-
-    class Dog : IAnimal, IComparable
-    {
-        private string name;
-        public Dog(string name)
-        {
-            this.Name = name;
-        }
-        public string Describe()
-        {
-            return "Hello, I'm a dog and my name is " + this.Name;
-        }
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-        public int CompareTo(object obj)
-        {
-            if (obj is IAnimal)
-                return this.Name.Equals((obj as IAnimal).Name) == true ? 1 : 0;
-            return 0;
-        }
-
-        public static bool CompareNames(object first, object second)
-        {
-            if (first is IAnimal && second is IAnimal)
-            {
-                if ((first as IAnimal).Name == (second as IAnimal).Name)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-
-    class testVirtual
-    {
-        public virtual void Foo()
-        {
-            Console.WriteLine("i do something you might not want me to.");
-        }
-    }
-
-    class Program : testVirtual, IMyInterface, IMultipleInheritance, Ijedan, Idva //absClass,IMyInterface, IMultipleInheritance,Ijedan,Idva
+    class Program : ProgramExtender //absClass,IMyInterface, IMultipleInheritance,Ijedan,Idva
     {
         static void Main(string[] args)
         {
@@ -115,56 +20,10 @@ namespace testInterfaces
             //AbstractFactoryTester();
             //PrototypeTester();
             //BuilderTester();
-            FacadeTester();
+            //FacadeTester();
+            //ProxyTester();
+
             Console.Read();
-        }
-
-        public override void Foo()
-        {
-            Console.WriteLine("now it does what you wait it to.");
-            // and maybe the thing that it already did ?
-            base.Foo();
-        }
-
-        public void MethodToImplement()
-        {
-            Console.WriteLine("MethodToImplement() called.");
-        }
-
-        public void ParentInterfaceMethod()
-        {
-            Console.WriteLine("ParentInterfaceMethod() called.");
-        }
-
-        public int wow()
-        {
-            return 1;
-        }
-
-        //public override void AbsMethod()
-        //{
-        //    Console.WriteLine("implemented");
-        //}
-
-        public void IzJedinice()
-        {
-            Console.WriteLine("one method from one interface");
-        }
-
-        void Idva.SameName()
-        {
-            Console.WriteLine("calling samename from idva");
-        }
-
-        void Ijedan.SameName()
-        {
-            Console.WriteLine("calling samename from ijedan");
-        }
-
-        int Idva.IzJedinice()
-        {
-            Console.WriteLine("one method from two interface");
-            return 1;
         }
 
         static void InterfacesTester() {
