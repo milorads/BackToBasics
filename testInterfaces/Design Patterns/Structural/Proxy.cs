c2a4e8e27e608590405f91b16ec2b181a94f725c
msekulovic <milorad.sekulovic@gmail.com>
2017-03-22 11:29:16 +0100

    added uml/text files and proxy classes

--- testInterfaces/Design Patterns/Structural/Proxy.cs
+++ testInterfaces/Design Patterns/Structural/Proxy.cs
@@ -6,7 +6,92 @@ using System.Threading.Tasks;
 
 namespace testInterfaces.Design_Patterns.Structural
 {
-    class Proxy
+    #region Structure code
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    abstract class Subject
     {
+        public abstract void Request();
     }
+
+    /// <summary>
+    /// The 'RealSubject' class
+    /// </summary>
+    class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Called RealSubject.Request()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Proxy' class
+    /// </summary>
+    class Proxy : Subject
+    {
+        private RealSubject _realSubject;
+
+        public override void Request()
+        {
+            // Use 'lazy initialization'
+            if (_realSubject == null)
+            {
+                _realSubject = new RealSubject();
+            }
+
+            _realSubject.Request();
+        }
+    }
+    #endregion
+
+    #region Implementation code
+    /// <summary>
+    /// The 'Subject interface
+    /// </summary>
+    public interface IMath
+    {
+        double Add(double x, double y);
+        double Sub(double x, double y);
+        double Mul(double x, double y);
+        double Div(double x, double y);
+    }
+
+    /// <summary>
+    /// The 'RealSubject' class
+    /// </summary>
+    class Math : IMath
+    {
+        public double Add(double x, double y) { return x + y; }
+        public double Sub(double x, double y) { return x - y; }
+        public double Mul(double x, double y) { return x * y; }
+        public double Div(double x, double y) { return x / y; }
+    }
+
+    /// <summary>
+    /// The 'Proxy Object' class
+    /// </summary>
+    class MathProxy : IMath
+    {
+        private Math _math = new Math();
+
+        public double Add(double x, double y)
+        {
+            return _math.Add(x, y);
+        }
+        public double Sub(double x, double y)
+        {
+            return _math.Sub(x, y);
+        }
+        public double Mul(double x, double y)
+        {
+            return _math.Mul(x, y);
+        }
+        public double Div(double x, double y)
+        {
+            return _math.Div(x, y);
+        }
+    }
+    #endregion
 }
