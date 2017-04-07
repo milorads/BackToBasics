464e46ed7b2325ac197beec4567075f7c2070c93
msekulovic <milorad.sekulovic@gmail.com>
2017-04-07 12:13:15 +0200

    added strategy pattern class

---
+++ testInterfaces/Design Patterns/Behavioral/Strategy/Strategy.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testInterfaces.Design_Patterns.Behavioral.Strategy
+{
+    #region Sample Code
+    /// <summary>
+    /// The 'Strategy' abstract class
+    /// </summary>
+    abstract class Strategy
+    {
+        public abstract void AlgorithmInterface();
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ConcreteStrategyA : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine(
+              "Called ConcreteStrategyA.AlgorithmInterface()");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ConcreteStrategyB : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine(
+              "Called ConcreteStrategyB.AlgorithmInterface()");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ConcreteStrategyC : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine(
+              "Called ConcreteStrategyC.AlgorithmInterface()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Context' class
+    /// </summary>
+    class Context
+    {
+        private Strategy _strategy;
+
+        // Constructor
+        public Context(Strategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void ContextInterface()
+        {
+            _strategy.AlgorithmInterface();
+        }
+    }
+    #endregion
+
+    #region Implementation Code
+    /// <summary>
+    /// The 'Strategy' abstract class
+    /// </summary>
+    abstract class SortStrategy
+    {
+        public abstract void Sort(List<string> list);
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class QuickSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            list.Sort(); // Default is Quicksort
+            Console.WriteLine("QuickSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ShellSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.ShellSort(); not-implemented
+            Console.WriteLine("ShellSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class MergeSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.MergeSort(); not-implemented
+            Console.WriteLine("MergeSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// The 'Context' class
+    /// </summary>
+    class SortedList
+    {
+        private List<string> _list = new List<string>();
+        private SortStrategy _sortstrategy;
+
+        public void SetSortStrategy(SortStrategy sortstrategy)
+        {
+            this._sortstrategy = sortstrategy;
+        }
+
+        public void Add(string name)
+        {
+            _list.Add(name);
+        }
+
+        public void Sort()
+        {
+            _sortstrategy.Sort(_list);
+
+            // Iterate over list and display results
+            foreach (string name in _list)
+            {
+                Console.WriteLine(" " + name);
+            }
+            Console.WriteLine();
+        }
+    }
+    #endregion
+}
