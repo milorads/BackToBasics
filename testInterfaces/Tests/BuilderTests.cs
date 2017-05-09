e76344e2b6493d1f39a809ccd266fb68522e417c
msekulovic <milorad.sekulovic@gmail.com>
2017-05-09 10:03:22 +0200

    added test methods to be rewritten into tests

---
+++ testInterfaces/Tests/BuilderTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using testInterfaces.Design_Patterns.Creational;
+
+namespace testInterfaces.Tests
+{
+    class BuilderTests
+    {
+        [Test]
+        public void TestBuilder()
+        {
+            var director = new Director();
+
+            Builder b1 = new ConcreteBuilder1();
+            Builder b2 = new ConcreteBuilder2();
+
+            director.Construct(b1);
+            var p1 = b1.GetResult();
+            p1.Show();
+
+            director.Construct(b2);
+            var p2 = b2.GetResult();
+            p2.Show();
+        }
+
+        [Test]
+        public void TestCarBuilder()
+        {
+            #region sample 2
+            VehicleBuilder builder;
+
+            var shop = new Shop();
+
+            builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+            #endregion
+        }
+    }
+}
