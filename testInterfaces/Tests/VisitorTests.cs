22ca744d5fc6ac7b9c1851f372084c25135ea0b4
milorads <milorad.sekulovic@gmail.com>
2017-07-06 12:41:09 -0400

    added classes for tests and included nunit with 2 template testing methods

---
+++ testInterfaces/Tests/VisitorTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace testInterfaces.Design_Patterns.Behavioral
+{
+    class VisitorTests
+    {
+        [Test]
+        static void ta()
+        {
+        }
+        [Test]
+        static void tb()
+        {
+        }
+    }
+}
