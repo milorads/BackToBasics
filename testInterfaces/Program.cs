66127ed2e1e59dddcbb19b5c36fb73413fd38567
milorads <milorad.sekulovic@gmail.com>
2017-07-07 11:57:42 -0400

    implemented mediator tests and method that uses it.

--- testInterfaces/Program.cs
+++ testInterfaces/Program.cs
@@ -38,8 +38,8 @@ namespace testInterfaces
             //CommandTester();//+
             //StrategyTester();//+
             //StateTester();//+
-            TemplateMethodTester();//t+
-            //MediatorTester();// -
+            //TemplateMethodTester();//+
+            MediatorTester();// -
             //ChainOfResponsibilityTester();// -
             //VisitorTester();// -
             //InterpreterTester();// -
@@ -697,7 +697,43 @@ namespace testInterfaces
 
         static void MediatorTester()
         {
+            #region sample 1
+            ConcreteMediator m = new ConcreteMediator();
 
+            ConcreteColleague1 c1 = new ConcreteColleague1(m);
+            ConcreteColleague2 c2 = new ConcreteColleague2(m);
+
+            m.Colleague1 = c1;
+            m.Colleague2 = c2;
+
+            c1.Send("How are you?");
+            c2.Send("Fine, thanks");
+            #endregion
+
+            #region sample 2
+            // Create chatroom
+            Chatroom chatroom = new Chatroom();
+
+            // Create participants and register them
+            Participant George = new Beatle("George");
+            Participant Paul = new Beatle("Paul");
+            Participant Ringo = new Beatle("Ringo");
+            Participant John = new Beatle("John");
+            Participant Yoko = new NonBeatle("Yoko");
+
+            chatroom.Register(George);
+            chatroom.Register(Paul);
+            chatroom.Register(Ringo);
+            chatroom.Register(John);
+            chatroom.Register(Yoko);
+
+            // Chatting participants
+            Yoko.Send("John", "Hi John!");
+            Paul.Send("Ringo", "All you need is love");
+            Ringo.Send("George", "My sweet Lord");
+            Paul.Send("John", "Can't buy me love");
+            John.Send("Yoko", "My sweet love");
+            #endregion
         }
 
         static void ChainOfResponsibilityTester()
