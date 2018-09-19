8c07cf1823feaa21abb66ea5b2963190ba3978c7
milorads <milorad.sekulovic@gmail.com>
2018-09-19 23:07:11 +0200

    red black tree

---
+++ BackToBasics/Topics/Data Structures/RedBlackTree.cs
@@ -0,0 +1,460 @@
+﻿using System;
+
+namespace BackToBasics.Topics.Data_Structures
+{
+    enum Color
+    {
+        Red,
+        Black
+    }
+    class RedBlackTree
+    {
+        /// <summary>
+        /// Object of type Node contains 4 properties
+        /// Colour
+        /// Left
+        /// Right
+        /// Parent
+        /// Data
+        /// </summary>
+        public class RedBlackNode
+        {
+            public Color colour;
+            public RedBlackNode left;
+            public RedBlackNode right;
+            public RedBlackNode parent;
+            public int data;
+
+            public RedBlackNode(int data) { this.data = data; }
+            public RedBlackNode(Color colour) { this.colour = colour; }
+            public RedBlackNode(int data, Color colour) { this.data = data; this.colour = colour; }
+        }
+        /// <summary>
+        /// Root node of the tree (both reference & pointer)
+        /// </summary>
+        private RedBlackNode root;
+        /// <summary>
+        /// New instance of a Red-Black tree object
+        /// </summary>
+        public RedBlackTree() { }
+        /// <summary>
+        /// Left Rotate
+        /// </summary>
+        /// <param name="X"></param>
+        /// <returns>void</returns>
+        private void LeftRotate(RedBlackNode X)
+        {
+            RedBlackNode Y = X.right; // set Y
+            X.right = Y.left;//turn Y's left subtree into X's right subtree
+            if (Y.left != null)
+            {
+                Y.left.parent = X;
+            }
+            if (Y != null)
+            {
+                Y.parent = X.parent;//link X's parent to Y
+            }
+            if (X.parent == null)
+            {
+                root = Y;
+            }
+            if (X == X.parent.left)
+            {
+                X.parent.left = Y;
+            }
+            else
+            {
+                X.parent.right = Y;
+            }
+            Y.left = X; //put X on Y's left
+            if (X != null)
+            {
+                X.parent = Y;
+            }
+
+        }
+        /// <summary>
+        /// Rotate Right
+        /// </summary>
+        /// <param name="Y"></param>
+        /// <returns>void</returns>
+        private void RightRotate(RedBlackNode Y)
+        {
+            // right rotate is simply mirror code from left rotate
+            RedBlackNode X = Y.left;
+            Y.left = X.right;
+            if (X.right != null)
+            {
+                X.right.parent = Y;
+            }
+            if (X != null)
+            {
+                X.parent = Y.parent;
+            }
+            if (Y.parent == null)
+            {
+                root = X;
+            }
+            if (Y == Y.parent.right)
+            {
+                Y.parent.right = X;
+            }
+            if (Y == Y.parent.left)
+            {
+                Y.parent.left = X;
+            }
+
+            X.right = Y;//put Y on X's right
+            if (Y != null)
+            {
+                Y.parent = X;
+            }
+        }
+        /// <summary>
+        /// Display Tree
+        /// </summary>
+        public void DisplayTree()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Nothing in the tree!");
+                return;
+            }
+            if (root != null)
+            {
+                InOrderDisplay(root);
+            }
+        }
+        /// <summary>
+        /// Find item in the tree
+        /// </summary>
+        /// <param name="key"></param>
+        public RedBlackNode Find(int key)
+        {
+            bool isFound = false;
+            RedBlackNode temp = root;
+            RedBlackNode item = null;
+            while (!isFound)
+            {
+                if (temp == null)
+                {
+                    break;
+                }
+                if (key < temp.data)
+                {
+                    temp = temp.left;
+                }
+                if (key > temp.data)
+                {
+                    temp = temp.right;
+                }
+                if (key == temp.data)
+                {
+                    isFound = true;
+                    item = temp;
+                }
+            }
+            if (isFound)
+            {
+                Console.WriteLine("{0} was found", key);
+                return temp;
+            }
+            else
+            {
+                Console.WriteLine("{0} not found", key);
+                return null;
+            }
+        }
+        /// <summary>
+        /// Insert a new object into the RB Tree
+        /// </summary>
+        /// <param name="item"></param>
+        public void Insert(int item)
+        {
+            RedBlackNode newItem = new RedBlackNode(item);
+            if (root == null)
+            {
+                root = newItem;
+                root.colour = Color.Black;
+                return;
+            }
+            RedBlackNode Y = null;
+            RedBlackNode X = root;
+            while (X != null)
+            {
+                Y = X;
+                if (newItem.data < X.data)
+                {
+                    X = X.left;
+                }
+                else
+                {
+                    X = X.right;
+                }
+            }
+            newItem.parent = Y;
+            if (Y == null)
+            {
+                root = newItem;
+            }
+            else if (newItem.data < Y.data)
+            {
+                Y.left = newItem;
+            }
+            else
+            {
+                Y.right = newItem;
+            }
+            newItem.left = null;
+            newItem.right = null;
+            newItem.colour = Color.Red;//colour the new node red
+            InsertFixUp(newItem);//call method to check for violations and fix
+        }
+        private void InOrderDisplay(RedBlackNode current)
+        {
+            if (current != null)
+            {
+                InOrderDisplay(current.left);
+                Console.Write("({0}) ", current.data);
+                InOrderDisplay(current.right);
+            }
+        }
+        private void InsertFixUp(RedBlackNode item)
+        {
+            //Checks Red-Black Tree properties
+            while (item != root && item.parent.colour == Color.Red)
+            {
+                /*We have a violation*/
+                if (item.parent == item.parent.parent.left)
+                {
+                    RedBlackNode Y = item.parent.parent.right;
+                    if (Y != null && Y.colour == Color.Red)//Case 1: uncle is red
+                    {
+                        item.parent.colour = Color.Black;
+                        Y.colour = Color.Black;
+                        item.parent.parent.colour = Color.Red;
+                        item = item.parent.parent;
+                    }
+                    else //Case 2: uncle is black
+                    {
+                        if (item == item.parent.right)
+                        {
+                            item = item.parent;
+                            LeftRotate(item);
+                        }
+                        //Case 3: recolour & rotate
+                        item.parent.colour = Color.Black;
+                        item.parent.parent.colour = Color.Red;
+                        RightRotate(item.parent.parent);
+                    }
+
+                }
+                else
+                {
+                    //mirror image of code above
+                    RedBlackNode X = null;
+
+                    X = item.parent.parent.left;
+                    if (X != null && X.colour == Color.Black)//Case 1
+                    {
+                        item.parent.colour = Color.Red;
+                        X.colour = Color.Red;
+                        item.parent.parent.colour = Color.Black;
+                        item = item.parent.parent;
+                    }
+                    else //Case 2
+                    {
+                        if (item == item.parent.left)
+                        {
+                            item = item.parent;
+                            RightRotate(item);
+                        }
+                        //Case 3: recolour & rotate
+                        item.parent.colour = Color.Black;
+                        item.parent.parent.colour = Color.Red;
+                        LeftRotate(item.parent.parent);
+
+                    }
+
+                }
+                root.colour = Color.Black;//re-colour the root black as necessary
+            }
+        }
+        /// <summary>
+        /// Deletes a specified value from the tree
+        /// </summary>
+        /// <param name="item"></param>
+        public void Delete(int key)
+        {
+            //first find the node in the tree to delete and assign to item pointer/reference
+            RedBlackNode item = Find(key);
+            RedBlackNode X = null;
+            RedBlackNode Y = null;
+
+            if (item == null)
+            {
+                Console.WriteLine("Nothing to delete!");
+                return;
+            }
+            if (item.left == null || item.right == null)
+            {
+                Y = item;
+            }
+            else
+            {
+                Y = TreeSuccessor(item);
+            }
+            if (Y.left != null)
+            {
+                X = Y.left;
+            }
+            else
+            {
+                X = Y.right;
+            }
+            if (X != null)
+            {
+                X.parent = Y;
+            }
+            if (Y.parent == null)
+            {
+                root = X;
+            }
+            else if (Y == Y.parent.left)
+            {
+                Y.parent.left = X;
+            }
+            else
+            {
+                Y.parent.left = X;
+            }
+            if (Y != item)
+            {
+                item.data = Y.data;
+            }
+            if (Y.colour == Color.Black)
+            {
+                DeleteFixUp(X);
+            }
+
+        }
+        /// <summary>
+        /// Checks the tree for any violations after deletion and performs a fix
+        /// </summary>
+        /// <param name="X"></param>
+        private void DeleteFixUp(RedBlackNode X)
+        {
+
+            while (X != null && X != root && X.colour == Color.Black)
+            {
+                if (X == X.parent.left)
+                {
+                    RedBlackNode W = X.parent.right;
+                    if (W.colour == Color.Red)
+                    {
+                        W.colour = Color.Black; //case 1
+                        X.parent.colour = Color.Red; //case 1
+                        LeftRotate(X.parent); //case 1
+                        W = X.parent.right; //case 1
+                    }
+                    if (W.left.colour == Color.Black && W.right.colour == Color.Black)
+                    {
+                        W.colour = Color.Red; //case 2
+                        X = X.parent; //case 2
+                    }
+                    else if (W.right.colour == Color.Black)
+                    {
+                        W.left.colour = Color.Black; //case 3
+                        W.colour = Color.Red; //case 3
+                        RightRotate(W); //case 3
+                        W = X.parent.right; //case 3
+                    }
+                    W.colour = X.parent.colour; //case 4
+                    X.parent.colour = Color.Black; //case 4
+                    W.right.colour = Color.Black; //case 4
+                    LeftRotate(X.parent); //case 4
+                    X = root; //case 4
+                }
+                else //mirror code from above with "right" & "left" exchanged
+                {
+                    RedBlackNode W = X.parent.left;
+                    if (W.colour == Color.Red)
+                    {
+                        W.colour = Color.Black;
+                        X.parent.colour = Color.Red;
+                        RightRotate(X.parent);
+                        W = X.parent.left;
+                    }
+                    if (W.right.colour == Color.Black && W.left.colour == Color.Black)
+                    {
+                        W.colour = Color.Black;
+                        X = X.parent;
+                    }
+                    else if (W.left.colour == Color.Black)
+                    {
+                        W.right.colour = Color.Black;
+                        W.colour = Color.Red;
+                        LeftRotate(W);
+                        W = X.parent.left;
+                    }
+                    W.colour = X.parent.colour;
+                    X.parent.colour = Color.Black;
+                    W.left.colour = Color.Black;
+                    RightRotate(X.parent);
+                    X = root;
+                }
+            }
+            if (X != null)
+                X.colour = Color.Black;
+        }
+        private RedBlackNode Minimum(RedBlackNode X)
+        {
+            while (X.left.left != null)
+            {
+                X = X.left;
+            }
+            if (X.left.right != null)
+            {
+                X = X.left.right;
+            }
+            return X;
+        }
+        private RedBlackNode TreeSuccessor(RedBlackNode X)
+        {
+            if (X.left != null)
+            {
+                return Minimum(X);
+            }
+            else
+            {
+                RedBlackNode Y = X.parent;
+                while (Y != null && X == Y.right)
+                {
+                    X = Y;
+                    Y = Y.parent;
+                }
+                return Y;
+            }
+        }
+
+        public void RedBlackOperations()
+        {
+            RedBlackTree tree = new RedBlackTree();
+            tree.Insert(5);
+            tree.Insert(3);
+            tree.Insert(7);
+            tree.Insert(1);
+            tree.Insert(9);
+            tree.Insert(-1);
+            tree.Insert(11);
+            tree.Insert(6);
+            tree.DisplayTree();
+            tree.Delete(-1);
+            tree.DisplayTree();
+            tree.Delete(9);
+            tree.DisplayTree();
+            tree.Delete(5);
+            tree.DisplayTree();
+        }
+    }
+}
