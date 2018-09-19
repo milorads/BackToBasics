7d0216ab5cb4e5c792910897d1545281df334f13
milorads <milorad.sekulovic@gmail.com>
2018-09-19 22:10:12 +0200

    bst

---
+++ BackToBasics/Topics/Data Structures/BinarySearchTree.cs
@@ -0,0 +1,36 @@
+﻿using BackToBasics.Helpers;
+
+namespace BackToBasics.Topics.Data_Structures
+{
+    class BinarySearchTree
+    {
+        public BinaryTreeNode Insert(BinaryTreeNode root, int v)
+        {
+            if (root == null)
+            {
+                root = new BinaryTreeNode {Data = v};
+            }
+            else if (v < root.Data)
+            {
+                root.Left = Insert(root.Left, v);
+            }
+            else
+            {
+                root.Right = Insert(root.Right, v);
+            }
+
+            return root;
+        }
+
+        public void traverse(BinaryTreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            traverse(root.Left);
+            traverse(root.Right);
+        }
+    }
+}
